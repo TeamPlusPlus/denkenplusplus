7b96e4f72a4e5b11b4e0bd2009a56aaa1aa06f79
Lukas Bestle <lukas@lu-x.me>
2013-06-02 10:46:36 +0200

    Add Open Graph

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="<?php echo $site->language(); ?>">
+<html lang="<?php echo $site->language(); ?>" prefix="og: http://ogp.me/ns#">
 	<head>
 		<meta charset="utf-8">
 		<title><?php echo $site->title(); ?> » <?php echo Episodes::title($page); ?></title>
@@ -7,6 +7,29 @@
 		<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0">
 		<meta name="apple-mobile-web-app-capable" content="yes">
 		<meta name="apple-mobile-web-app-status-bar-style" content="black">
+		
+		<meta property="og:title" content="<?php echo Episodes::title($page); ?>">
+		<meta property="og:type" content="audio.podcast">
+		<meta property="og:url" content="<?php echo $page->url(); ?>">
+		<?php if(Episodes::infos($page)->image): ?>
+		<meta property="og:image" content="<?php echo Episodes::infos($page)->image['url'] ?>">
+		<meta property="og:image:type" content="image/jpeg">
+		<?php else: ?>
+		<meta property="og:image" content="http://media.plusplus.serpens.uberspace.de/Images/Denken++/profile.png">
+		<meta property="og:image:type" content="image/png">
+		<?php endif; ?>
+		<meta property="og:image:width" content="1000">
+		<meta property="og:image:height" content="1000">
+		<?php if(Episodes::infos($page)->mp3): ?>
+		<meta property="og:audio" content="<?php echo Episodes::infos($page)->mp3['url'] ?>">
+		<meta property="og:audio:type" content="audio/mpeg">
+		<?php endif; ?>
+		<?php if($page->subtitle()): ?>
+		<meta property="og:description" content="<?php echo $page->subtitle() ?>">
+		<?php endif; ?>
+		<meta property="og:determiner" content="the">
+		<meta property="og:locale" content="de_DE">
+		<meta property="og:site_name" content="<?php echo $page->title() ?>">
 		
 		<link href="<?php echo url('/feed/mp3') ?>" rel="alternate" type="application/rss+xml" title="Podcast-Feed (MP3)">
 		<link href="<?php echo url('/feed/m4a') ?>" rel="alternate" type="application/rss+xml" title="Podcast-Feed (M4A)">
