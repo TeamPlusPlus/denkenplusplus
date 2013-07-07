fec01999c0f7508d585e11334cc1f075af0e7c14
Lukas Bestle <lukas@lu-x.me>
2013-07-07 13:15:41 +0200

    Extract assets and site to submodules

--- site/templates/episod.php
+++
@@ -1,6 +0,0 @@
-<?php if(!r::is('ajax')) snippet('header') ?>
-<?php snippet('menu') ?>
-
-<?php echo snippet('episode', array('p' => $page)); ?>
-
-<?php if(!r::is('ajax')) snippet('footer') ?>
--- site/templates/redirect.php
+++
@@ -1,6 +0,0 @@
-<?php if(!r::is('ajax')) {
-	go($page->link());
-} else {
-	echo 'Redirect: ' . $page->link();
-}
-?>
