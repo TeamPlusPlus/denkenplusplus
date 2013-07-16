11135feb8384a08cfc1329c02a52830a016c33cd
Lukas Bestle <lukas@lu-x.me>
2013-07-16 16:12:56 +0200

    Update to outsourced assets

--- .deploy.sh
+++ .deploy.sh
@@ -1,7 +1,8 @@
 # Compile stuff
 /var/www/virtual/plusplus/libs/sass/bin/sass --update assets/scss/main.scss:assets/css/main.css -f -t compressed
-/var/www/virtual/plusplus/libs/coffee/bin/coffee -c --output assets/js assets/coffee/*
-/var/www/virtual/plusplus/libs/uglify/bin/uglifyjs assets/js/main.js -o assets/js/main.js
+
+/var/www/virtual/plusplus/libs/coffee/bin/coffee -c --output assets/base/js assets/base/coffee/*
+/var/www/virtual/plusplus/libs/uglify/bin/uglifyjs assets/base/js/ajax.js -o assets/js/main.js
 
 # Remove the .sass-cache folder - who needs that xD
 rm -R .sass-cache
